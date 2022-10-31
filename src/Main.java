81fdc7a7b937e10b589ad87ad0c1507c3ee983dd
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-10-31 15:19:40 -0400

    added JavaDocs for Person class

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Person person = new Person("Tom", 30);
+        var person = new Person("Tom", 30);
         person.saysHello();
 
         System.out.println(person);
--- src/Person.java
+++ src/Person.java
@@ -6,27 +6,62 @@ public class Person
 
     // Getters and Setters
 
+    /**
+     * This method returns the Person object's name field
+     * @return
+     */
     public String getName()
     {
         return m_name;
     }
 
+    /**
+     * This method sets the Person object's name field
+     * @param name
+     */
     public void setName(String name)
     {
         m_name = name;
     }
 
+    /**
+     * This method returns the Person object's age field
+     * @return
+     */
     public int getAge()
     {
         return m_age;
     }
 
+    /**
+     * This method sets the Person object's age field
+     * @param age
+     */
     public void setAge(int age)
     {
         m_age = age;
     }
 
+    /**
+     * This method sets both the Person object's name and age fields
+     * @param name {String}
+     * @param age {int}
+     */
+    public void set(String name, int age)
+    {
+        setName(name);
+        setAge(age);
+    }
+
     // Constructor
+
+
+    /**
+     * Person parameterized Constructor that takes both name and age
+     *
+     * @param name {String}
+     * @param age {int}
+     * */
     public Person(String name, int age)
     {
         m_name = name;
@@ -34,6 +69,10 @@ public class Person
     }
 
     // Public Methods
+
+    /**
+     * This method outputs the Person's name + " says Hello!"
+     */
     public void saysHello()
     {
         System.out.println(getName() + " says Hello!");
@@ -42,6 +81,11 @@ public class Person
     // Private Methods
 
     // Overridden Methods
+
+    /**
+     * This method overrides the built-in toString method belonging to the Object superclass
+     * @return
+     */
     @Override
     public String toString()
     {
